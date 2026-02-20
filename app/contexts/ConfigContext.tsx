09720f018c77b7a131cf714d6747058dcb7b8650
noodlebenji2960 <noodlebenji2960@gmail.com>
2026-02-20 16:13:09 +0100

    .

--- app/contexts/ConfigContext.tsx
+++ app/contexts/ConfigContext.tsx
@@ -1,13 +1,25 @@
 import React, { createContext, useContext } from "react";
-import featureConfig from "@/data/feature-config.json"
+
+//override
+const useDevOverride = true;
+
+const featureConfig =
+  useDevOverride || process.env.NODE_ENV !== "production"
+    ? require("@/data/feature-config-development.json")
+    : require("@/data/feature-config.json");
 
 // Create the context
 const ConfigContext = createContext(featureConfig);
 
 // Provider component
-export const ConfigProvider = ({ children, config = featureConfig }) => {
+export const ConfigProvider = ({ children, config }) => {
+  // Use the passed config if provided, otherwise fallback to environment-based config
+  const finalConfig = config || featureConfig;
+
   return (
-    <ConfigContext.Provider value={config}>{children}</ConfigContext.Provider>
+    <ConfigContext.Provider value={finalConfig}>
+      {children}
+    </ConfigContext.Provider>
   );
 };
 
